f9ef9a0e761e99214e011859dcb05ba9a9c8a2d3
raretet <rassandev200777@.com>
2022-10-22 18:17:32 +0300

    feet(asdsad):asdasd

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./index.css";
-
-const Something = () => {
-  return <div></div>;
-};
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Something />
+    <App/>
   </React.StrictMode>
 );
