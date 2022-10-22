9b3dd2a5f2c94eba94f7d76f8c9aba0fd32272fc
gStukunov <mr.stukunov@gmail.com>
2022-10-23 02:07:35 +0300

    game page + training

--- my-app/src/pages/GamePage/game-page.tsx
+++ my-app/src/pages/GamePage/game-page.tsx
@@ -1,20 +1,55 @@
 import { FC, useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
+import Sabath from "../../assets/images/characters/sabath.svg";
+import Dungeon from "../../assets/images/characters/dungeon.svg";
+
 import styles from "./game-page.module.scss";
+import { mainStore } from "../../store/main-store";
 
 const supabase = createClient(
   "https://mnumahwmrmdklgvvmtlf.supabase.co/",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1udW1haHdtcm1ka2xndnZtdGxmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjYzNjA0MzYsImV4cCI6MTk4MTkzNjQzNn0.P4BIRTYA51C2ikll17z7Ju9RfvQoERwZLTV3xa78yT8"
 );
 
-export const GamePage: FC = () => {
-  const [questions, setQuestions] = useState();
+type IconsPaths = {
+  sabath: { name: string; path: string };
+  dungeon: { name: string; path: string };
+};
 
-  const [health, setHealth] = useState();
-  const [happines, setHappines] = useState();
-  const [money, setMoney] = useState();
-  const [hunger, setHunger] = useState();
+const icons: IconsPaths = {
+  sabath: { name: "Sabath(Сабатх)", path: Sabath },
+  dungeon: { name: "Ван ДаркХолм", path: Dungeon },
+};
+
+const skipTraining = {
+  question: "Хочешь ли пропустить обучение?",
+  answer1: "все понятно...",
+  answer2: "нуууу...",
+};
+
+const training = {
+  description: "",
+};
+
+export const GamePage: FC = () => {
+  const [questions, setQuestions] = useState(null);
+  const [training, setTraining] = useState<boolean>(true);
+  const [age, setAge] = useState<number>(0);
+
+  const [health, setHealth] = useState<number>(mainStore.health);
+  const [happines, setHappines] = useState<number>(mainStore.happiness);
+  const [money, setMoney] = useState<number>(mainStore.money);
+  const [hunger, setHunger] = useState<number>(mainStore.hunger);
+
+  const ageIncrementer = (currIncr: number) => {
+    setAge(age + currIncr);
+  };
+
+  const endTraining = () => {
+    ageIncrementer(18);
+    setTraining(false);
+  };
 
   useEffect(() => {
     const request = async () => {
@@ -26,7 +61,28 @@ export const GamePage: FC = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper}></div>
+      <div className={styles.wrapper}>
+        <div className={styles.characteristics}>
+          <div>properties</div>
+        </div>
+        {training && (
+          <div className={styles.question}>
+            <div className={styles.iconWrap}>
+              <img src={icons.sabath.path} className={styles.icon} />
+            </div>
+            <div>{icons.sabath.name}</div>
+            <div>{skipTraining.question}</div>
+            <div>
+              <button onClick={endTraining}>{skipTraining.answer1}</button>
+              <button onClick={endTraining}>{skipTraining.answer2}</button>
+            </div>
+          </div>
+        )}
+        <div className={styles.characteristics}>
+          <div className={styles.age}>{age}</div>
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
