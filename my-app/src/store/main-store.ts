ddbacbf35b0748b049c4842130e0fb771dea9b73
gStukunov <mr.stukunov@gmail.com>
2022-10-23 05:11:43 +0300

    aboba

--- my-app/src/store/main-store.ts
+++ my-app/src/store/main-store.ts
@@ -1,7 +1,7 @@
 import { makeAutoObservable } from "mobx";
 
 class MainStore {
-  difficultyCurrent = 'Eazy';
+  difficultyCurrent = "Eazy";
 
   constructor() {
     makeAutoObservable(this);
@@ -11,54 +11,50 @@ class MainStore {
     this.difficultyCurrent = difficulty;
   }
 
-  health = 0
-  hunger = 0
-  money = 0
-  happiness = 0
+  health = 0;
+  hunger = 0;
+  money = 0;
+  happiness = 0;
 
   easy() {
-      if (this.difficultyCurrent === 'Eazy') {
-          this.health = 100
-          this.hunger = 100
-          this.money = 100
-          this.happiness = 100
-      }
-  }
+    if (this.difficultyCurrent === "Rich") {
+      this.health = 100;
+      this.hunger = 100;
+      this.money = 100;
+      this.happiness = 100;
+    }
 
-  medium() {
-      if (this.difficultyCurrent === 'Middle') {
-          this.health = 70
-          this.hunger = 70
-          this.money = 70
-          this.happiness = 70
-      }
-  }
+    if (this.difficultyCurrent === "Middle") {
+      this.health = 70;
+      this.hunger = 70;
+      this.money = 70;
+      this.happiness = 70;
+    }
 
-  hard() {
-      if (this.difficultyCurrent === 'Rich') {
-          this.health = 40
-          this.hunger = 40
-          this.money = 40
-          this.happiness = 40
-      }
+    if (this.difficultyCurrent === "Eazy") {
+      this.health = 40;
+      this.hunger = 40;
+      this.money = 40;
+      this.happiness = 40;
+    }
   }
 
   // Buffs
 
-  education = false
-  relationships = false
-  credit = false
+  education = false;
+  relationships = false;
+  credit = false;
 
   getEducation(buff: boolean) {
-    this.education = buff
+    this.education = buff;
   }
 
   getRelationships(buff: boolean) {
-    this.relationships = buff
+    this.relationships = buff;
   }
 
   getCredit(buff: boolean) {
-    this.credit = buff
+    this.credit = buff;
   }
 }
 
